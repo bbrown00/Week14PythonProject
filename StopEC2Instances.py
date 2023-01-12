ea9e3db406f56db125c65fbd0ae3e7a200a1d39f
bbrown00 <116729775+bbrown00@users.noreply.github.com>
2023-01-12 11:56:05 -0500

    Update StopEC2Instances.py

--- StopEC2Instances.py
+++ StopEC2Instances.py
@@ -1,3 +1,22 @@
+#Creating and running a ECS instance though python boto 3
+
+import boto3
+
+client = boto3.client('ec2')
+
+response = client.run_instances(
+    ImageId='ami-0b5eea76982371e91',
+    InstanceType='t2.micro',
+    MinCount= 1,
+    MaxCount= 1,
+    KeyName='Week14KeyPair'
+)
+
+
+for instance in response ['Instances']:
+    print("Instance ID created is : {} Instance Type is :{}".format(instance['InstanceId'],instance['InstanceType']))
+
+
 #Stop Ec2 Instances
 
 import boto3
